2617b61e6abc6f1eeef2b6ec14c7be0d156685cb
Luis Pedraza <luispedraza@gmail.com>
2021-01-02 19:55:02 +0100

    Primer commit de la aplicación de prueba, con algunos fig en la pantalla principal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
 
+const GIFS = [
+  'https://media3.giphy.com/media/3o7527pa7qs9kCG78A/200w.webp?cid=ecf05e4734yrzfcmi5oo2xxflnwricrd9poh9nm43fydieoo&rid=200w.webp',
+  'https://media4.giphy.com/media/4Zo41lhzKt6iZ8xff9/giphy.webp?cid=ecf05e4734yrzfcmi5oo2xxflnwricrd9poh9nm43fydieoo&rid=giphy.webp',
+  'https://media1.giphy.com/media/51Uiuy5QBZNkoF3b2Z/200w.webp?cid=ecf05e4734yrzfcmi5oo2xxflnwricrd9poh9nm43fydieoo&rid=200w.webp',
+  
+]
+
+const OTHER_GIFS = [
+  'https://media4.giphy.com/media/vFKqnCdLPNOKc/200.webp?cid=ecf05e47sdg03ccdwtdpjbqscjy7otmgccyqio9gv0yetkhv&rid=200.webp',
+  'https://media1.giphy.com/media/CqVNwrLt9KEDK/200.webp?cid=ecf05e47sdg03ccdwtdpjbqscjy7otmgccyqio9gv0yetkhv&rid=200.webp',
+  
+]
+
 function App() {
+  const [gifs, setGifs] = useState(GIFS);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section className="App-content">
+    
+    {
+      gifs.map(g => <img src={g} alt="Gif encontrado" /> ) 
+    }
+    
+    <button onClick={() => setGifs(OTHER_GIFS)}>Cambiar GIFS</button>
+    
+    </section>
     </div>
-  );
-}
-
-export default App;
+    );
+  }
+  
+  export default App;
+  
